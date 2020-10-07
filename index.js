2a367d3a742b991347bfb8e40a65c453dbc14957
Joyal Thomas <joyal@dexlock.com>
2020-10-07 15:08:35 +0530

    updated to print hello world from 2 different files

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 var express = require('express')
 var app = express()
+
+const helloRoute = require('./hello');
+const worldRoute = require("./world");
+
+app.use("/hello",helloRoute)
+app.use("/world",worldRoute)
+
+
+
+
+
+
+
+const port = 3000
+
+
+
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
